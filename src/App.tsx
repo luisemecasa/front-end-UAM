a65c8853f875054242ba6f2b7ce99fa5e7b02e2e
luisemecasa <luism.canons@autonoma.edu.co>
2024-08-23 17:11:28 -0500

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Principal from './components/Principal'
+
+
+function App() {
+  return(
+    <>
+      <Header />
+      <Principal />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Principal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Principal.css'
+
+export default function Principal() {
+  return (
+    <main className="main">
+        <aside className="aside">
+            This is the aside
+        </aside>
+        <section className="principal">
+            This is the principal content 
+        </section>
+    </main>
+  )
+}
